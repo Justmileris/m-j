5456ab5927e5de17d146417eff0fff6e0be4f1f2
Justmileris <Justmileris@gmail.com>
2017-12-22 01:55:30 +0200

    Add files via upload
    Let's have some fun with colors!

---
+++ script.js
@@ -0,0 +1,35 @@
+var input = "";
+var timeoutId = 0;
+
+function theMostImportantMainFunction() {
+	console.log('Hello, World!');
+}
+
+function letsHaveSomeFun() {
+	document.onkeypress = function(e) {
+		
+	clearTimeout(timeoutId);
+    timeoutId = setTimeout(resetInput, 700);
+		
+		e = e || window.event;
+		var magic = (typeof e.which == "number") ? e.which : e.keyCode;
+		
+		
+		if (magic) {
+			
+			input += String.fromCharCode(magic);	
+			document.getElementById('text').style.color=input;
+			document.body.style.background=input;
+		
+		}
+	};
+}
+
+function resetInput() {
+	input = "";
+}
+
+
+
+theMostImportantMainFunction();
+letsHaveSomeFun();
